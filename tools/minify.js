e31acad1e087ea354ad5951314c41ae97f603496
Igor Laborie <ilaborie@gmail.com>
2019-08-24 19:14:15 +0200

    fix pdf generation 2

--- tools/minify.js
+++ tools/minify.js
@@ -1,20 +1,27 @@
 const {readFileSync, writeFileSync} = require('fs');
 const {sync: glob} = require('glob');
 const {minify} = require('html-minifier');
-const {Logger} = require('plop-logger');
+const {Logger, LogLevel} = require('plop-logger');
 const {colorEmojiConfig} = require('plop-logger/lib/extra/colorEmojiConfig');
 
 Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('minifier');
+logger.level = LogLevel.All;
 
-glob(`public/**/*.html`)
-    .forEach(file => {
-        const html = readFileSync(file, 'utf8');
-        const minified = minify(html, {});
-        const gain = html.length - minified.length;
-        if (gain > 0) {
-            const percent = (gain / html.length) * 100;
-            logger.info(file, () => ['gain', percent.toFixed(2), '%'].join(" "));
-            writeFileSync(file, minified, {flag: 'w'});
-        }
-    });
+const htmlFiles = `public/**/*.html`;
+logger.info('Compress HTML files', htmlFiles);
+
+const totalGain = glob(htmlFiles)
+  .map(file => {
+    const html = readFileSync(file, 'utf8');
+    const minified = minify(html, {});
+    const gain = html.length - minified.length;
+    if (gain > 0) {
+      const percent = (gain / html.length) * 100;
+      logger.debug(file, () => ['gain', percent.toFixed(2), '%'].join(" "));
+      writeFileSync(file, minified, {flag: 'w'});
+    }
+    return gain;
+  }).reduce((acc, elt) => acc + elt, 0);
+
+logger.info('Total gain', '' + totalGain);
